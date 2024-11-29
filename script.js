d665bc1c7bf8b03932ac8e32f260f30a7969ab09
ImShubham09 <singhashubham612@gmail.com>
2024-11-29 23:24:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-//your code here
 /* Styling for the main element */
 main.accio {
     width: 150px;
     height: 150px;
-    background-color: #000000;
+    background-color: #000000; /* Black background */
 }
 
 /* Styling for the paragraph element */
 p.hello-accio {
-    color: #ffffff;
-    margin-left: 32px;
-    padding: 10px;
+    color: rgb(255, 255, 255); /* Ensure color matches 'rgb' format expected by Cypress */
+    margin-left: 32px;         /* Left margin */
+    padding: 10px;             /* Padding on all sides */
 }
