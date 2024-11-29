285869fcaf5327fedae30093061787325f76e0a8
ImShubham09 <singhashubham612@gmail.com>
2024-11-29 23:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+/* Styling for the main element */
+main.accio {
+    width: 150px;
+    height: 150px;
+    background-color: #000000;
+}
+
+/* Styling for the paragraph element */
+p.hello-accio {
+    color: #ffffff;
+    margin-left: 32px;
+    padding: 10px;
+}
